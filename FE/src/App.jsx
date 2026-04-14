1f442fe8134ca9027e416469973c9d3deb150480
wkdalswn11 <wkdalswn194@gamil.com>
2026-04-14 09:40:35 +0900

    chore: 헤더확인

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -4,12 +4,12 @@ import CompanyList from "./pages/company/list";
 import InvestmentList from "./pages/investment/list";
 import "./styles/table.css";
 import Result from "./pages/compare/result";
+import Header from "./components/layout/header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Header />
       <CompanyList />
       <InvestmentList />
       <Result />
