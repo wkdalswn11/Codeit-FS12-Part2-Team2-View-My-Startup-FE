8bfe1e232261c53f3fb668e32bbf41983b59fc1b
wkdalswn11 <wkdalswn194@gamil.com>
2026-04-14 18:23:35 +0900

    FEAT:로그인여부에 따른 라우트 분기

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,23 +1,44 @@
 import { useState } from "react";
 import "./App.css";
-import Auth from "./pages/auth/Auth";
-import CompanyList from "./pages/company/list";
 import InvestmentList from "./pages/investment/list";
 import "./styles/table.css";
 import Result from "./pages/compare/result";
 import Header from "./components/layout/header";
 import CompanyPage from "./pages/company/CompanyPage";
 import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedLayout from "./components/layout/ProtextedLayout";
+import Auth from "./pages/auth/Auth";
 
 function App() {
   return (
     <>
       <Header />
-      <Auth />
       <Routes>
-        <Route path="/" element={<CompanyPage />} />
-        <Route path="/investments" element={<InvestmentList />} />
-        <Route path="/compare" element={<Result />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedLayout title="전체 스타트업 목록">
+              <CompanyPage />
+            </ProtectedLayout>
+          }
+        />
+        <Route
+          path="/investments"
+          element={
+            <ProtectedLayout title="투자 현황">
+              <InvestmentList />
+            </ProtectedLayout>
+          }
+        />
+        <Route
+          path="/compare"
+          element={
+            <ProtectedLayout title="비교 현황">
+              <Result />
+            </ProtectedLayout>
+          }
+        />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </>
   );
