c4ac8681c10603281b76a6a420f8944f44a4b7aa
wkdalswn11 <wkdalswn194@gamil.com>
2026-04-14 16:36:26 +0900

    feat:pagination 컴포넌트 작성, company api 연결

---
+++ FE/src/pages/company/CompanyPage.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import List from "./list";
+import Pagination from "../../components/pagination/Pagination";
+import "../../styles/pagination.css";
+
+const CompanyPage = () => {
+  const [companyList, setCompanyList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [meta, setMeta] = useState({
+    page: 1,
+    limit: 10,
+    total: 0,
+    totalPages: 1,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `http://localhost:8080/companies?page=${currentPage}&limit=10`,
+      );
+      const result = await res.json();
+
+      setCompanyList(result.data || []);
+      setMeta(result.meta || {});
+    };
+    fetchData();
+  }, [currentPage]);
+
+  return (
+    <>
+      <List companyList={companyList} />
+      <Pagination
+        currentPage={meta.page}
+        totalPages={meta.totalPages}
+        onPageChange={setCurrentPage}
+      />
+    </>
+  );
+};
+
+export default CompanyPage;
