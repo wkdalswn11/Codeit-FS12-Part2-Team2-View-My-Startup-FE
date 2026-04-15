81e9ef29a8cc2ee3f61627defd80009868ada146
dahee <dahee5121@gmail.com>
2026-04-15 16:28:07 +0900

    feat : 버튼 ui 만들기

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,21 +1,46 @@
 import { useState } from "react";
 import "./App.css";
-import CompanyList from "./pages/company/list";
-import InvestmentList from "./pages/investment/list";
 import "./styles/table.css";
 import Result from "./pages/compare/result";
+import ComparePage from "./pages/compare/ComparePage";
 import Header from "./components/layout/header";
 import CompanyPage from "./pages/company/CompanyPage";
 import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedLayout from "./components/layout/ProtextedLayout";
+import Auth from "./pages/auth/Auth";
+import InvestmentPage from "./pages/investment/InvestmentPage";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<CompanyPage />} />
-        <Route path="/investments" element={<InvestmentList />} />
-        <Route path="/compare" element={<Result />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedLayout title="전체 스타트업 목록">
+              <CompanyPage />
+            </ProtectedLayout>
+          }
+        />
+        <Route
+          path="/investments"
+          element={
+            <ProtectedLayout title="투자 현황">
+              <InvestmentPage />
+            </ProtectedLayout>
+          }
+        />
+        <Route
+          path="/compare"
+          element={
+            <ProtectedLayout title="비교 현황">
+              {/* <Result /> */}
+              <ComparePage />
+            </ProtectedLayout>
+          }
+        />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </>
   );
--- FE/src/components/ui/Button.jsx
+++ FE/src/components/ui/Button.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Button.css";
 
-const Button = () => {
-  return <div>Button</div>;
-};
+function Button({
+  children,
+  type = "Button-large",
+  variant = "Button-primary",
+  icon,
+  ...props
+}) {
+  // type: Button-large, Button-medium, Button-outline
+  // variant: Button-primary, Button-gray, Button-primary-outline, Button-outline-orange, Button-outline-gray
+  return (
+    <button className={`Button-btn ${type} ${variant}`} {...props}>
+      {icon && <span>{icon}</span>}
+      {children}
+    </button>
+  );
+}
 
 export default Button;
