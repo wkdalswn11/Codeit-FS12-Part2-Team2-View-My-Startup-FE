2cbb1accfaa9ba812addd7911dfb97f12f7e1a3c
wkdalswn11 <wkdalswn194@gamil.com>
2026-04-22 17:13:18 +0900

    refactor: 이미지 파일을 assets/images 폴더로 이동

--- FE/src/components/layout/header.jsx
+++ FE/src/components/layout/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import "../../../react.css";
-import logo from "./img/logo1.png";
+import logo from "../../assets/images/logo1.png";
 import { Link, NavLink } from "react-router-dom";
 import useUserStore from "../../store/userStore";
 function Header() {
